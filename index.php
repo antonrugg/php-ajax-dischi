64faa200dcbbc5f9a49fb34188c568f0e0a91f15
Antonio Ruggiero <antonrugg@gmail.com>
2022-05-05 16:06:46 +0200

    milestone 2 complete - index.html | working on milestone 1 - convert in php - index.php

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
-<?php
-    
-
-
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../php-ajax-dischi/styles/styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js" defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js"
+        integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
+    <script src="./vue.js" defer></script>
+    <script src="./script/script.js" defer></script>
+    <title>PHP AJAX Dischi</title>
+</head>
+<body>
+    <header>
+            <div id="logo-container">
+                <img src="./images/spotify-logo.png" alt="Fake Spotify" />
+            
+            
+            </div>
+    </header>
+</body>
+</html>
