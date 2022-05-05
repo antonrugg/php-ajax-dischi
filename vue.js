f2e8a576923c81082d15df8c37876109b2da7e6a
Antonio Ruggiero <antonrugg@gmail.com>
2022-05-05 13:54:30 +0200

    init commit - links, scripts added - vue, axios, fonts in index.html

---
+++ vue.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el: '#app',
+    data() {
+        return{
+            test: 'testtesttest',
+        }
+    },
+
+    
+})
+
+
+
+// author: "Bon Jovi"
+// genre: "Rock"
+// poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg"
+// title: "New Jersey"
+// year: "1988"
+
